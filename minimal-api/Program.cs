662958c7e800a7403c37ff285fd6f571a09d3047
Matheus Pizani <mpizani28@gmail.com>
2025-07-12 11:50:08 -0300

    iniciando projeto

---
+++ minimal-api/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (minimal_api.Dominio.DTOs.LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+        return Results.Ok("Login realizado com sucesso");
+    else
+        return Results.Unauthorized();
+});
+
+app.Run();
