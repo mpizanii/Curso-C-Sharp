a780fa0d964999d4e24379959a1db2e7edefbdf0
Matheus Pizani <mpizani28@gmail.com>
2025-07-12 21:19:22 -0300

    criando seed para cadastrar adm

--- minimal-api/Infraestrutura/Db/DbContexto.cs
+++ minimal-api/Infraestrutura/Db/DbContexto.cs
@@ -8,6 +8,19 @@ namespace minimal_api.Infraestrutura.Db
     {
         private readonly IConfiguration _configuracaoAppSettings;
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Administrador>().HasData(
+                new Administrador
+                {
+                    Id = 1,
+                    Email = "adm@teste.com",
+                    Senha = "123456",
+                    Perfil = "Adm"
+                }
+            );
+        }
+
         public DbContexto(IConfiguration configuracaoAppSettings)
         {
             _configuracaoAppSettings = configuracaoAppSettings;
