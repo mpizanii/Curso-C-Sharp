2818ea9dec649caaa4a7afd252c577f4525cffee
Matheus Pizani <mpizani28@gmail.com>
2025-07-13 11:44:39 -0300

    criando modelo de veiculos

---
+++ minimal-api/Dominio/Interfaces/IVeiculoServico.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace minimal_api.Dominio.Interfaces
+{
+    public interface IVeiculoServico
+    {
+        List<Veiculo> Todos(int pagina = 1, string nome = null, string marca = null);
+        Veiculo? BuscaPorId(int id);
+        void Incluir(Veiculo veiculo);
+        void Atualizar(Veiculo veiculo);
+        void Apagar(Veiculo veiculo);
+    }
+}
--- minimal-api/Infraestrutura/Db/DbContexto.cs
+++ minimal-api/Infraestrutura/Db/DbContexto.cs
@@ -27,6 +27,7 @@ namespace minimal_api.Infraestrutura.Db
         }
 
         public DbSet<Administrador> Administradores { get; set; } = default!;
+        public DbSet<Veiculo> Veiculos { get; set; } = default!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
