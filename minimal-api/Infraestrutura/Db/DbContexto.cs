97891fbd8e0ee64da72975baacb860b4f61f0a1c
Matheus Pizani <mpizani28@gmail.com>
2025-07-12 21:09:45 -0300

    configurando o ef e criando a tabela administradores

--- minimal-api/Infraestrutura/Db/DbContexto.cs
+++ minimal-api/Infraestrutura/Db/DbContexto.cs
@@ -1,7 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration; // IMPORTANTE
+using minimal_api.Dominio.Entidades;
+
 namespace minimal_api.Infraestrutura.Db
 {
-    public class DbContexto
+    public class DbContexto : DbContext
     {
-        
+        private readonly IConfiguration _configuracaoAppSettings;
+
+        public DbContexto(IConfiguration configuracaoAppSettings)
+        {
+            _configuracaoAppSettings = configuracaoAppSettings;
+        }
+
+        public DbSet<Administrador> Administradores { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql");
+                if (!string.IsNullOrEmpty(stringConexao))
+                {
+                    optionsBuilder.UseMySql(
+                        stringConexao,
+                        ServerVersion.AutoDetect(stringConexao)
+                    );
+                }
+            }
+        }
     }
 }
--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,9 +1,22 @@
+using minimal_api.Infraestrutura.Db;
+using minimal_api.Dominio.DTOs;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (minimal_api.Dominio.DTOs.LoginDTO loginDTO) =>
+app.MapPost("/login", (LoginDTO loginDTO) =>
 {
     if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
         return Results.Ok("Login realizado com sucesso");
