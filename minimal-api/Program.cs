29e51a55049a5e172e10280cf9f6d913630c3fba
Matheus Pizani <mpizani28@gmail.com>
2025-07-12 21:45:34 -0300

    validando adm com login e senha

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,8 +1,13 @@
 using minimal_api.Infraestrutura.Db;
 using minimal_api.Dominio.DTOs;
 using Microsoft.EntityFrameworkCore;
+using minimal_api.Dominio.Interfaces;
+using minimal_api.Dominio.Servicos;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
 
 builder.Services.AddDbContext<DbContexto>(options =>
 {
@@ -16,9 +21,9 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (LoginDTO loginDTO) =>
+app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
-    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    if (administradorServico.Login(loginDTO) != null)
         return Results.Ok("Login realizado com sucesso");
     else
         return Results.Unauthorized();
