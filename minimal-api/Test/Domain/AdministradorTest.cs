647b3cb1a37dbb84552cba1b02bd017e95b5278b
Matheus Pizani <mpizani28@gmail.com>
2025-07-16 08:44:49 -0300

    criando teste de unidade para o modelo adm

---
+++ minimal-api/Test/Domain/AdministradorTest.cs
@@ -0,0 +1,23 @@
+using minimal_api.Dominio.Entidades;
+
+namespace Test.Domain.Entidades;
+
+[TestClass]
+public class AdministradorTest
+{
+    [TestMethod]
+    public void TestarGetSetPropriedades()
+    {
+        var adm = new Administrador();
+
+        adm.Id = 1;
+        adm.Email = "teste@teste.com";
+        adm.Senha = "teste";
+        adm.Perfil = "Adm";
+
+        Assert.AreEqual(1, adm.Id);
+        Assert.AreEqual("teste@teste.com", adm.Email);
+        Assert.AreEqual("teste", adm.Senha);
+        Assert.AreEqual("Adm", adm.Perfil);
+    }
+}
