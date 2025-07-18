ab8fa95ea1023beff3a6b49060861d166c6bbd32
Matheus Pizani <mpizani28@gmail.com>
2025-07-17 21:59:28 -0300

    criando testes de persistencia

---
+++ minimal-api/Test/Domain/Servicos/AdministradorServico.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using minimal_api.Dominio.Entidades;
+using minimal_api.Dominio.Servicos;
+using minimal_api.Infraestrutura.Db;
+
+namespace Test.Domain.Entidades;
+
+[TestClass]
+public class AdministradorServicoTest
+{
+    private DbContexto CriarContextoDeTeste()
+    {
+        var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var path = Path.GetFullPath(Path.Combine(assemblyPath ?? "", "..", "..", ".."));
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(path ?? Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        var configuration = builder.Build();
+
+        return new DbContexto(configuration);
+    }
+
+
+    [TestMethod]
+    public void TestandoSalvarAdministrador()
+    {
+        var context = CriarContextoDeTeste();
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE Administradores");
+
+        var adm = new Administrador();
+        adm.Email = "teste@teste.com";
+        adm.Senha = "teste";
+        adm.Perfil = "Adm";
+
+        var administradorServico = new AdministradorServico(context);
+
+        administradorServico.Incluir(adm);
+
+        Assert.AreEqual(1, administradorServico.Todos(1).Count());
+    }
+
+    [TestMethod]
+    public void TestandoBuscaPorId()
+    {
+        var context = CriarContextoDeTeste();
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE Administradores");
+
+        var adm = new Administrador();
+        adm.Email = "teste@teste.com";
+        adm.Senha = "teste";
+        adm.Perfil = "Adm";
+
+        var administradorServico = new AdministradorServico(context);
+
+        administradorServico.Incluir(adm);
+        var admDoBanco = administradorServico.BuscaPorId(adm.Id);
+
+        Assert.AreEqual(1, admDoBanco?.Id);
+    }
+}
